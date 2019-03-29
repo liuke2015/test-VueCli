297fcb6e9b10c043faba3e3c1861e93ff3d20ed8
liuke2015 <376060613@qq.com>
2019-03-29 10:25:49 +0800

    新增：vuex-2、vuex触发mutations与传参

---
+++ src/components/testVuex/vuexTemp/vuexMutations.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="vuexMutations">
+    <h1>vuex触发mutations与传参</h1>
+    <div>vuex的count={{count}}</div>
+   <!-- <button @click="$store.commit('plus')">+</button>-->
+    <button @click="plus({stemp:5})">+</button>
+    <button @click="reduce">-</button>
+  </div>
+</template>
+
+<script>
+  import {mapState,mapMutations} from "vuex"
+  export default {
+    name: "vuexMutations",  
+    computed:mapState(["count"]),
+    /*方法一*/
+     methods: mapMutations([
+       "plus","reduce"
+     ])
+  }
+</script>
+
+<style lang="less">
+  #vuexMutations {
+    button {
+      margin-top: 20px;
+    }
+  }
+</style>
